4a3170ff524669a1a22d3d0cf3684213e368a4d2
niharika25boe10048-cmyk <niharika.25boe10048@vitbhopal.ac.in>
2026-06-15 01:01:54 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,16 +1,36 @@
-function getCart(){
+<!DOCTYPE html>
+<html>
+<body>
 
-return JSON.parse(
-localStorage.getItem("cart")
-) || [];
+<button onclick="addItem()">Add Item</button>
 
+<script>
+function getCart() {
+    try {
+        return JSON.parse(localStorage.getItem("cart")) || [];
+    } catch (e) {
+        return [];
+    }
 }
 
-function saveCart(cart){
-
-localStorage.setItem(
-"cart",
-JSON.stringify(cart)
-);
-
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
+
+function addItem() {
+    let cart = getCart();
+
+    cart.push({
+        name: "Milk",
+        qty: 2,
+        price: 30
+    });
+
+    saveCart(cart);
+
+    console.log(getCart());
+}
+</script>
+
+</body>
+</html>
